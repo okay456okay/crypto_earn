c81b4a39153917ecfabb4b05257a1205f321b4c2
朱秀龙 <xiulong.zhu@casstime.com>
2025-03-04 09:36:13 +0800

    添加导入设置

--- config_example.py
+++ config_example.py
@@ -6,6 +6,21 @@ api_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 proxies = {
     # 'http': 'socks5://127.0.0.1:7890',
     # 'https': 'socks5://127.0.0.1:7890',
+    # 如果不用代理，把下面这两行注释掉
     'http': 'http://127.0.0.1:7890',
     'https': 'http://127.0.0.1:7890',
 }
+
+import logging
+# 配置日志
+logging.basicConfig(
+    level=logging.INFO,
+    # format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format="[%(asctime)-15s] %(name)s %(levelname)s (%(funcName)s(), %(filename)s:%(lineno)d): %(message)s",
+    handlers=[
+        logging.FileHandler("crypto_yield_monitor.log"),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger("crypto_yield_monitor")
