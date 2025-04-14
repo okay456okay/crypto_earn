0f6b2907f6468198ee928c89a7c5f2a0de4e6467
朱秀龙 <xiulong.zhu@casstime.com>
2025-04-14 10:28:25 +0800

    添加快速启动重开仓的脚本

--- scripts/positions.sh
+++ scripts/positions.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+
+cd $(dirname $0)
+
 source ../venv/bin/activate
 
- python ../trade/gateio_positions.py
- python ../trade/bitget_positions.py
- python ../trade/bybit_positions.py
+python ../trade/gateio_positions.py
+python ../trade/bitget_positions.py
+python ../trade/bybit_positions.py
