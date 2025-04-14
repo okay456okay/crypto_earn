33ef22351efbb097cf2e8ba36785c3fc24b2ecba
朱秀龙 <xiulong.zhu@casstime.com>
2025-04-15 00:07:25 +0800

    放宽筛选范围

--- scripts/funding_bybit.sh
+++ scripts/funding_bybit.sh
@@ -9,4 +9,7 @@ token=$1
 
 
 script_dir=$(dirname $0)
-nohup ${script_dir}/../venv/bin/python $script_dir/../trade/bybit_funding_arbitrage.py -s $token/USDT &
+
+if ! ps auxww|grep $token |grep bybit_funding_arbitrage|grep -v grep; then
+  nohup ${script_dir}/../venv/bin/python $script_dir/../trade/bybit_funding_arbitrage.py -s $token/USDT &
+fi
