0bf77a1ac36b21a265b0824a1f70118e27b7b27c
朱秀龙 <xiulong.zhu@casstime.com>
2025-04-15 08:25:38 +0800

    修改positions.sh脚本

--- scripts/positions.sh
+++ scripts/positions.sh
@@ -2,11 +2,11 @@
 
 
 cd $(dirname $0)
+mkdir -p reports
 
 source ../venv/bin/activate
 
 suffix=$(date +%Y%m%d%H%M)
-
 python ../trade/gateio_positions.py >reports/gateio_positions_$suffix.log
 python ../trade/bitget_positions.py >reports/bitget_positions_$suffix.log
 python ../trade/bybit_positions.py >reports/bybit_positions_$suffix.log
