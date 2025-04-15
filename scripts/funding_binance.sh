70232841818929a1db25e7feee1ac24f50e03be7
朱秀龙 <xiulong.zhu@casstime.com>
2025-04-15 23:36:45 +0800

    添加binance自动重开仓脚本

---
+++ scripts/funding_binance.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $(basename $0) token"
+    exit 1
+fi
+
+token=$1
+
+
+script_dir=$(dirname $0)
+
+if ! ps auxww|grep $token |grep -v grep|grep -q binance_funding_arbitrage; then
+  nohup ${script_dir}/../venv/bin/python $script_dir/../trade/binance_funding_arbitrage.py -s $token/USDT &>/dev/null &
+fi
