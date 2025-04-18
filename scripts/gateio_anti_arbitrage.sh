2564cc31a265aab3ee1827f0f3f76f93a8b9640b
朱秀龙 <xiulong.zhu@casstime.com>
2025-04-18 23:30:20 +0800

    修复gateio资金费率反套利脚本

--- scripts/gateio_anti_arbitrage.sh
+++ scripts/gateio_anti_arbitrage.sh
@@ -7,4 +7,4 @@ script_dir=$(dirname $0)
 ps auxww|grep -v grep|grep  gateio_anti_funding_rate.py|awk '{print $2}'|xargs kill
 
 cd $script_dir
-nohup ${script_dir}/../venv/bin/python $script_dir/../trade/gateio_anti_funding_rate.py -t -0.5 -l 5 &>reports/antibybit-$(date +%Y-%m-%d-%H) &
+nohup ${script_dir}/../venv/bin/python $script_dir/../trade/gateio_anti_funding_rate.py -t -0.01 -l 6 &>reports/antibybit-$(date +%Y-%m-%d-%H) &
