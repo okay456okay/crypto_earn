2b36c21df0443ff6d345d7a633753819202807a9
朱秀龙 <xiulong.zhu@casstime.com>
2025-04-22 18:05:36 +0800

    优化吃费率脚本

---
+++ scripts/bybit_eat_arbitrage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 脚本名称: bybit_eat_arbitrage.sh
+
+
+script_dir=$(dirname $0)
+ps auxww|grep -v grep|grep  bybit_eat_funding_rate.py|awk '{print $2}'|xargs kill
+
+cd $script_dir
+nohup ${script_dir}/../venv/bin/python $script_dir/../trade/bybit_eat_funding_rate.py -t -0.5 &>reports/eatbybit-$(date +%Y-%m-%d-%H) &
