50761172e0453e0f97829a1f580dd332ab7d97c9
朱秀龙 <xiulong.zhu@casstime.com>
2025-04-23 16:22:20 +0800

    去掉7秒比较脚本，影响速度

--- scripts/bybit_eat_arbitrage.sh
+++ scripts/bybit_eat_arbitrage.sh
@@ -7,5 +7,5 @@ script_dir=$(dirname $0)
 ps auxww|grep -v grep|grep  bybit_eat_funding_rate.py|awk '{print $2}'|xargs kill
 
 cd $script_dir
-nohup ${script_dir}/../venv/bin/python $script_dir/../trade/bybit_eat_funding_rate.py -t -0.3 &>reports/eatbybit-$(date +%Y-%m-%d-%H) &
-nohup ${script_dir}/../venv/bin/python $script_dir/../trade/bybit_eat_funding_rate.py -t -0.3 -o 6 &>reports/eatbybit7-$(date +%Y-%m-%d-%H) &
+nohup ${script_dir}/../venv/bin/python $script_dir/../trade/bybit_eat_funding_rate.py -t -0.5 &>reports/eatbybit-$(date +%Y-%m-%d-%H) &
+# nohup ${script_dir}/../venv/bin/python $script_dir/../trade/bybit_eat_funding_rate.py -t -0.3 -o 6 &>reports/eatbybit7-$(date +%Y-%m-%d-%H) &
