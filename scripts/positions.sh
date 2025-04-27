12303b18c26789c17675347d2a7af4bfb46c4e09
朱秀龙 <xiulong.zhu@casstime.com>
2025-04-27 11:52:11 +0800

    添加自动申购理财

--- scripts/positions.sh
+++ scripts/positions.sh
@@ -16,7 +16,7 @@ mkdir -p reports
 source ../venv/bin/activate
 
 suffix=$(date +%Y%m%d%H%M)
-python ../trade/gateio_positions.py >reports/gateio_positions_$suffix.log
+python ../trade/gateio_positions.py |grep -v 'gateio_positions.py' >reports/gateio_positions_$suffix.log
 python ../trade/bitget_positions.py >reports/bitget_positions_$suffix.log
 python ../trade/bybit_positions.py >reports/bybit_positions_$suffix.log
 python ../trade/binance_positions.py >reports/binance_positions_$suffix.log
