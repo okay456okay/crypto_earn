f562d0df99136e721d60cd1ab252b856e774d7e5
朱秀龙 <xiulong.zhu@casstime.com>
2025-05-03 22:46:50 +0800

    fix positions.sh

--- scripts/positions.sh
+++ scripts/positions.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # 脚本名称: positions.sh
 # 用途: 查看各交易所的持仓情况
@@ -7,20 +7,19 @@
 #   ./positions.sh -d     # 启用调试模式
 # 注意: 该脚本会显示Gate.io、Binance、Bitget和Bybit的持仓信息
 
-#!/usr/bin/env bash
-
-
 cd $(dirname $0)
-mkdir -p reports
 
 source ../venv/bin/activate
 
+cd ../trade
+mkdir -p reports
+
 suffix=$(date +%Y%m%d%H%M)
-python ../trade/gateio_positions.py |grep -v 'gateio_positions.py' >reports/gateio_positions_$suffix.log
-python ../trade/bitget_positions.py >reports/bitget_positions_$suffix.log
-python ../trade/bybit_positions.py >reports/bybit_positions_$suffix.log
-python ../trade/binance_positions.py >reports/binance_positions_$suffix.log
-python ../trade/exchange_position_arbitrage.py >reports/positions_sum_$suffix.log
+python gateio_positions.py |grep -v 'gateio_positions.py' >reports/gateio_positions_$suffix.log
+python bitget_positions.py >reports/bitget_positions_$suffix.log
+python bybit_positions.py >reports/bybit_positions_$suffix.log
+python binance_positions.py >reports/binance_positions_$suffix.log
+python exchange_position_arbitrage.py >reports/positions_sum_$suffix.log
 
 cd reports
 cat gateio_positions_$suffix.log bitget_positions_$suffix.log bybit_positions_$suffix.log binance_positions_$suffix.log positions_sum_$suffix.log >positions
