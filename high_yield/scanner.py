4b1e18c21d19e4b1c96dd9d7d270b3b7c300e46a
朱秀龙 <xiulong.zhu@casstime.com>
2025-05-26 09:38:47 +0800

    修复几个小问题

--- high_yield/scanner.py
+++ high_yield/scanner.py
@@ -382,23 +382,28 @@ class CryptoYieldMonitor:
             # 获取所有交易所的活期理财产品
             binance_products = self.exchange_api.get_binance_flexible_products()
             products = binance_products
-            logger.info(f"从{products[0]['exchange']}获取到{len([i for i in products if i['duration'] ==0])}个活期理财和{len([i for i in products if i['duration'] > 0])}定期理财产品")
+            if products:
+                logger.info(f"从{products[0]['exchange']}获取到{len([i for i in products if i['duration'] ==0])}个活期理财和{len([i for i in products if i['duration'] > 0])}定期理财产品")
 
             gateio_products = self.exchange_api.get_gateio_flexible_products()
             products = gateio_products
-            logger.info(f"从{products[0]['exchange']}获取到{len([i for i in products if i['duration'] ==0])}个活期理财和{len([i for i in products if i['duration'] > 0])}定期理财产品")
+            if products:
+                logger.info(f"从{products[0]['exchange']}获取到{len([i for i in products if i['duration'] ==0])}个活期理财和{len([i for i in products if i['duration'] > 0])}定期理财产品")
 
             bitget_products = self.exchange_api.get_bitget_flexible_products()
             products = bitget_products
-            logger.info(f"从{products[0]['exchange']}获取到{len([i for i in products if i['duration'] ==0])}个活期理财和{len([i for i in products if i['duration'] > 0])}定期理财产品")
+            if products:
+                logger.info(f"从{products[0]['exchange']}获取到{len([i for i in products if i['duration'] ==0])}个活期理财和{len([i for i in products if i['duration'] > 0])}定期理财产品")
 
             bybit_products = self.exchange_api.get_bybit_flexible_products()
             products = bybit_products
-            logger.info(f"从{products[0]['exchange']}获取到{len([i for i in products if i['duration'] ==0])}个活期理财和{len([i for i in products if i['duration'] > 0])}定期理财产品")
+            if products:
+                logger.info(f"从{products[0]['exchange']}获取到{len([i for i in products if i['duration'] ==0])}个活期理财和{len([i for i in products if i['duration'] > 0])}定期理财产品")
 
             okx_products = self.exchange_api.get_okx_flexible_products()
             products = okx_products
-            logger.info(f"从{products[0]['exchange']}获取到{len([i for i in products if i['duration'] ==0])}个活期理财和{len([i for i in products if i['duration'] > 0])}定期理财产品")
+            if products:
+                logger.info(f"从{products[0]['exchange']}获取到{len([i for i in products if i['duration'] ==0])}个活期理财和{len([i for i in products if i['duration'] > 0])}定期理财产品")
 
             # 合并所有产品
             all_products = binance_products + bitget_products + bybit_products + gateio_products + okx_products
