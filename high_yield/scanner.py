35b6c25dbdba4c1e4ba4bb1233ac2a886d8af395
朱秀龙 <xiulong.zhu@casstime.com>
2025-08-03 17:22:48 +0800

    理财添加最新现货价格信息

--- high_yield/scanner.py
+++ high_yield/scanner.py
@@ -289,10 +289,6 @@ class CryptoYieldMonitor:
                 continue
 
             # 生成通知内容
-            try:
-                print(product['price'])
-            except Exception as e:
-                print(product)
             notification = {
                 "exchange": product["exchange"],
                 "token": token,
